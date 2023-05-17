5a919b1fa0e605cc48712ac86e13e343b0e14b9d
ghanteyyy <ghanteyyy@gmail.com>
2023-05-17 11:52:06 +0545

    Added url patterns for signup, signin and displaying model test

--- Users/urls.py
+++ Users/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('test/', views.Eg)
+    path('', views.Index, name='index'),
+    path('login/', views.Login, name='login'),
+    path('signup/', views.SignUp, name='signup'),
+    path('modeltest/', views.TakeModelTest, name='modeltest'),
 ]

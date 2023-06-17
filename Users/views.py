8d947298f85a8f4e2198767176f6a2e7768fd0fd
ghanteyyy <ghanteyyy@gmail.com>
2023-06-17 23:41:59 +0545

    Updated views for signup, logout and dashboard

--- Users/views.py
+++ Users/views.py
@@ -1,7 +1,11 @@
 import os
 import json
+import datetime
 from pathlib import Path
-from django.shortcuts import render
+from .models import CustomUser
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 
 def SignUp(request):
@@ -9,18 +13,78 @@ def SignUp(request):
         'login': False
     }
 
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        dob_year = int(request.POST['dob-year'])
+        dob_month = int(request.POST['dob-month'])
+        dob_day = int(request.POST['dob-day'])
+
+        date = datetime.date(dob_year, dob_month, dob_day)
+        profile_image_path = request.FILES['uploaded-profile-image']
+
+        gender = request.POST['gen']
+
+        newUser = CustomUser(
+            email=email,
+            Gender=gender,
+            DOB=date,
+            ProfileImage=profile_image_path
+        )
+
+        newUser.set_password(password)
+        newUser.save()
+
+        user = authenticate(request, email=email, password=password)
+        login(request, user)
+
+        return redirect('/dashboard')
+
     return render(request, 'Signup.html', conditions)
 
 
 def Login(request):
-    conditions = {
-        'login': True
-    }
+    if request.user.id is None:
+        conditions = {
+            'login': True
+        }
+
+        if request.method == 'POST':
+            email = request.POST['email']
+            password = request.POST['password']
+
+            user = authenticate(request, email=email, password=password)
+
+            if user is not None:
+                login(request, user)
+
+                if user.is_superuser:
+                    return redirect('/admin')
+
+                return redirect('/dashboard')
+
+            else:
+                messages.error(request, 'Email and Password did not match')
+
+    else:
+        return redirect('/dashboard')
+
     return render(request, 'Signup.html', conditions)
 
 
 def Index(request):
-    return render(request, 'index.html')
+    details = {
+        'request': request,
+    }
+
+    if request.user.id:
+        details.update({'nav_template': 'WelcomeNav.html'})
+
+    else:
+        details.update({'nav_template': 'IndexNav.html'})
+
+    return render(request, 'index.html', details)
 
 
 def TakeModelTest(request):
@@ -53,3 +117,18 @@ def TakeModelTest(request):
     }
 
     return render(request, 'ModelTest.html', values)
+
+
+def DashBoard(request):
+    print(request.user.ProfileImage)
+    if request.user.id:
+        return render(request, 'DashBoard.html')
+
+    else:
+        return redirect('login')
+
+
+def Logout(request):
+    logout(request)
+
+    return redirect('index')

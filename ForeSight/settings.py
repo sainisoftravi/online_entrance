d4991a136fa555d92b411a1da49473aa0f7f604d
ghanteyyy <ghanteyyy@gmail.com>
2023-06-17 23:24:49 +0545

    Added media url, media root and auth user model

--- ForeSight/settings.py
+++ ForeSight/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
@@ -124,4 +127,5 @@ STATICFILES_DIRS = [
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+AUTH_USER_MODEL = 'Users.CustomUser'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'

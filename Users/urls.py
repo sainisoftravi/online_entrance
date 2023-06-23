36f6beb928233fdb3d316ec859c6e0cd75cc4e65
ghanteyyy <ghanteyyy@gmail.com>
2023-06-24 01:18:39 +0545

    Updated url patterns

--- Users/urls.py
+++ Users/urls.py
@@ -5,7 +5,11 @@ urlpatterns = [
     path('', views.Index, name='index'),
     path('login/', views.Login, name='login'),
     path('signup/', views.SignUp, name='signup'),
-    path('dashboard/', views.DashBoard, name='dashboard'),
-    path('modeltest/', views.TakeModelTest, name='modeltest'),
-    path('logout/', views.Logout, name='logout')
+    path('logout/', views.Logout, name='logout'),
+    path('profile/', views.Profile, name='profile'),
+    path('deleteaccount/', views.DeleteAccount, name='deleteaccount'),
+    path('updateprofile/', views.UpdateProfile, name='updateprofile'),
+    path('modeltest/<program>', views.TakeModelTest, name='modeltest'),
+    path('updatepassword/', views.UpdatePassword, name='updatepassword'),
+    path('programselector/', views.ProgramSelector, name='programselector'),
 ]

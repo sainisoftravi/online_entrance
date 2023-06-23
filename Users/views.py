97bfffd0d01f77a1ee5cfa5117e64b838058b328
ghanteyyy <ghanteyyy@gmail.com>
2023-06-24 01:22:18 +0545

    Added new views

--- Users/views.py
+++ Users/views.py
@@ -1,11 +1,12 @@
-import os
-import json
+import random
 import datetime
-from pathlib import Path
 from .models import CustomUser
+from django.conf import Settings
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import check_password
 from django.contrib.auth import authenticate, login, logout
+from .models import Programme, Subject, Questions, CustomUser
 
 
 def SignUp(request):
@@ -21,17 +22,23 @@ def SignUp(request):
         dob_month = int(request.POST['dob-month'])
         dob_day = int(request.POST['dob-day'])
 
-        date = datetime.date(dob_year, dob_month, dob_day)
-        profile_image_path = request.FILES['uploaded-profile-image']
-
         gender = request.POST['gen']
+        date = datetime.date(dob_year, dob_month, dob_day)
+
+        if 'uploaded-profile-image' in request.FILES:
+            profile_image_path = request.FILES['uploaded-profile-image']
+
+        else:
+            profile_image_path = None
 
         newUser = CustomUser(
             email=email,
             Gender=gender,
-            DOB=date,
-            ProfileImage=profile_image_path
+            DOB=date
         )
+
+        if profile_image_path:
+            newUser.ProfileImage = profile_image_path
 
         newUser.set_password(password)
         newUser.save()
@@ -39,7 +46,7 @@ def SignUp(request):
         user = authenticate(request, email=email, password=password)
         login(request, user)
 
-        return redirect('/dashboard')
+        return redirect('/profile')
 
     return render(request, 'Signup.html', conditions)
 
@@ -53,22 +60,31 @@ def Login(request):
         if request.method == 'POST':
             email = request.POST['email']
             password = request.POST['password']
+            remember_me = request.POST.get('remember-me', False)
 
             user = authenticate(request, email=email, password=password)
 
             if user is not None:
                 login(request, user)
 
+                if remember_me is False:
+                    request.session.set_expiry(0)
+
                 if user.is_superuser:
                     return redirect('/admin')
 
-                return redirect('/dashboard')
+                redirect_url = request.session.get('next', '/')
+
+                if 'next' in request.session:
+                    del request.session['next'] # Clear the session variable
+
+                return redirect(redirect_url)
 
             else:
                 messages.error(request, 'Email and Password did not match')
 
     else:
-        return redirect('/dashboard')
+        return redirect('/profile')
 
     return render(request, 'Signup.html', conditions)
 
@@ -78,7 +94,10 @@ def Index(request):
         'request': request,
     }
 
-    if request.user.id:
+    if request.user.is_superuser:
+        return redirect('/admin')
+
+    elif request.user.id:
         details.update({'nav_template': 'WelcomeNav.html'})
 
     else:
@@ -87,48 +106,95 @@ def Index(request):
     return render(request, 'index.html', details)
 
 
-def TakeModelTest(request):
-    questions = dict()
+def TakeModelTest(request, program):
+    tests = []
+    values = dict()
+    programme = Programme.objects.filter(Name=program)[0]
 
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    jsonPath = os.path.join(BASE_DIR, 'static', 'Questions.json')
+    for subject in Subject.objects.filter(ProgrammeID=programme):
+        questions = list(Questions.objects.filter(SubjectID=subject))
+        random.shuffle(questions)
 
-    with open(jsonPath) as f:
-        contents = json.load(f)
+        questions = questions[:subject.TotalQuestionsToSelect]
 
-    # all_contents = contents['BCA']['English']
-    # all_contents.update(contents['BCA']['Math'])
+        for question in questions:
+            choices = [question.OptionOne, question.OptionTwo, question.OptionThree, question.OptionFour]
+            random.shuffle(choices)
 
-    # for i in range(200):
-    #     all_keys = list(all_contents.keys())
-    #     question = random.choice(all_keys)
+            details = {
+                'id': question.ID,
+                'title': question.Title,
+                'choices': choices
+            }
 
-    #     questions[question] = all_contents[question]['choices']
-    #     all_contents.pop(question)
+            tests.append(details)
 
-    all_contents = contents['BCA']['Math']
+    for test in tests:
+        values[test['title']] = test['choices']
 
-    for question, values in all_contents.items():
-        questions[question] = values['choices']
-
-    values = {
-        'nums': list(range(100)),
-        'questions': questions
-    }
-
-    return render(request, 'ModelTest.html', values)
+    return render(request, 'ModelTest.html', {'questions': values})
 
 
-def DashBoard(request):
+def ProgramSelector(request):
+    if request.user.is_authenticated:
+        allPrograms = []
+
+        for programObj in Programme.objects.all():
+            allPrograms.append(programObj.Name)
+
+        return render(request, 'ProgramSelector.html', {'programs': allPrograms})
+
+    else:
+        request.session['next'] = 'programselector'
+        return redirect('login')
+
+
+def Profile(request):
     print(request.user.ProfileImage)
     if request.user.id:
-        return render(request, 'DashBoard.html')
+        return render(request, 'Profile.html')
 
     else:
         return redirect('login')
 
 
+def UpdateProfile(request):
+    user = CustomUser.objects.get(id=request.user.id)
+    user.ProfileImage = request.FILES['uploaded-profile-image']
+    user.save()
+
+    return redirect('/profile')
+
+
+def UpdatePassword(request):
+    user = CustomUser.objects.get(id=request.user.id)
+
+    old_password_encrypted = user.password
+    old_password = request.POST['OldPassword']
+
+    if check_password(old_password, old_password_encrypted):
+        new_password = request.POST['NewPassword']
+        user.set_password(new_password)
+
+        messages.success(request, 'Password Changed Successfully')
+
+    else:
+        messages.error(request, 'Old Password did not matched')
+
+    return redirect('profile')
+
+
 def Logout(request):
+    logout(request)
+
+    return redirect('index')
+
+
+def DeleteAccount(request):
+    user = CustomUser.objects.get(id=request.user.id)
+    user.is_active = False
+    user.save()
+
     logout(request)
 
     return redirect('index')

8181fef890e67a4f7593f27e0233315c5e0ef9b9
ghanteyyy <ghanteyyy@gmail.com>
2023-06-27 19:00:20 +0545

    Mapped new urls

--- Users/models.py
+++ Users/models.py
@@ -1,40 +1,56 @@
 import uuid
+import datetime
 from django.db import models
 from django.utils.timezone import now
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
+from .utils import GenerateRandomURL
 
 
 def user_directory_path(instance, filename):
     # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+
     return f'{instance.id}/profile/{filename}'
 
 
 class CustomUserManager(BaseUserManager):
-    """Define a model manager for User model with no username field."""
+    """
+    Define a model manager for User model with no username field
+    """
 
     def _create_user(self, email, password=None, **extra_fields):
-        """Create and save a User with the given email and password."""
+        """
+        Create and save a User with the given email and password
+        """
+
         if not email:
             raise ValueError('The given email must be set')
+
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
+
         return user
 
     def create_user(self, email, password=None, **extra_fields):
+
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
+
         return self._create_user(email, password, **extra_fields)
 
     def create_superuser(self, email, password=None, **extra_fields):
-        """Create and save a SuperUser with the given email and password."""
+        """
+        Create and save a SuperUser with the given email and password
+        """
+
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
+
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
@@ -66,16 +82,89 @@ class CustomUser(AbstractUser):
             )
 
     MemberSince = models.DateTimeField(
-                null=False,
-                blank=False,
-                default=now,
-                editable=False,
-            )
+                    null=False,
+                    blank=False,
+                    default=now,
+                    editable=False,
+                )
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
+
+
+class Results(models.Model):
+    class Meta:
+        verbose_name_plural = "Results"
+
+    UserID = models.ForeignKey(
+            "CustomUser",
+            on_delete = models.CASCADE
+        )
+
+    ID = models.UUIDField(
+            primary_key=True,
+            default=uuid.uuid4,
+            editable=False
+        )
+
+    ProgrammeName = models.CharField(
+                        null = False,
+                        blank = False,
+                        max_length = 10
+                    )
+
+    CorrectCounter = models.SmallIntegerField(
+                        null = False,
+                        blank = False,
+                        default = 0
+                    )
+
+    Slug = models.SlugField(
+                        null = False,
+                        blank = False,
+                        unique = True
+                    )
+
+    Date = models.DateField(
+                null=False,
+                blank=False,
+                editable=False,
+                default=datetime.date.today
+            )
+
+    def save(self, *args, **kwargs):
+        self.Slug = GenerateRandomURL('Result')
+
+        super(Results, self).save(*args, **kwargs)
+
+
+class ResultDetails(models.Model):
+    class Meta:
+        verbose_name_plural = "ResultDetails"
+
+    ResultID = models.ForeignKey(
+            "Results",
+            on_delete = models.CASCADE
+        )
+
+    ID = models.UUIDField(
+            primary_key=True,
+            default=uuid.uuid4,
+            editable=False
+        )
+
+    QuestionID = models.ForeignKey(
+                    "Questions",
+                    on_delete = models.CASCADE
+                )
+
+    UserAnswer = models.CharField(
+                    null = False,
+                    blank = False,
+                    max_length = 20
+                )
 
 
 class Programme(models.Model):
@@ -141,34 +230,34 @@ class Questions(models.Model):
     Title = models.TextField(
                 null = False,
                 blank = False
-        )
+            )
 
     Answer = models.CharField(
                 null = False,
                 blank = False,
                 max_length = 20
-        )
+            )
 
     OptionOne = models.CharField(
                     null = False,
                     blank = False,
                     max_length = 20
-        )
+                )
 
     OptionTwo = models.CharField(
                     null = False,
                     blank = False,
                     max_length = 20
-        )
+                )
 
     OptionThree = models.CharField(
                     null = False,
                     blank = False,
                     max_length = 20
-        )
+                )
 
     OptionFour = models.CharField(
                     null = False,
                     blank = False,
                     max_length = 20
-        )
+                )
--- Users/urls.py
+++ Users/urls.py
@@ -7,10 +7,12 @@ urlpatterns = [
     path('signup/', views.SignUp, name='signup'),
     path('logout/', views.Logout, name='logout'),
     path('profile/', views.Profile, name='profile'),
+    path('history/', views.ShowHistory, name='history'),
     path('getresult/', views.GetResult, name='getresult'),
     path('deleteaccount/', views.DeleteAccount, name='deleteaccount'),
     path('updateprofile/', views.UpdateProfile, name='updateprofile'),
     path('modeltest/<program>', views.TakeModelTest, name='modeltest'),
     path('updatepassword/', views.UpdatePassword, name='updatepassword'),
     path('programselector/', views.ProgramSelector, name='programselector'),
+    path('detailed-result/<slug:slug>', views.DetailedHistory, name='detailed-history'),
 ]

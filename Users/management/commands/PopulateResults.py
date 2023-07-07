52c58b9a94dfdf0470835de575b27c0f63fb3ee2
ghanteyyy <ghanteyyy@gmail.com>
2023-07-07 18:05:25 +0545

    Populate results with inputted credentials

--- Users/management/commands/PopulateResults.py
+++ Users/management/commands/PopulateResults.py
@@ -4,10 +4,13 @@ from Users.models import CustomUser, Programme, Subject, Questions, Results, Res
 
 
 class PopulateResults:
-    def __init__(self, Prog):
+    def __init__(self, Prog, Email, NumberOfResultsToGenerate):
+        self.Email = Email
         self.Programme = Prog
-        self.ProgrammeObj = Programme.objects.filter(Name=self.Programme)[0]
-        self.UsersObj = CustomUser.objects.filter(email='ghanteyyy@gmail.com')[0]
+        self.NumberOfResultsToGenerate = NumberOfResultsToGenerate
+
+        self.ProgrammeObj = Programme.objects.filter(Name=self.Programme).first()
+        self.UsersObj = CustomUser.objects.filter(email=self.Email).first()
 
     def GetSubjects(self):
         SubjectsID = []
@@ -18,46 +21,46 @@ class PopulateResults:
         return SubjectsID
 
     def Action(self):
-        correct_counter = 0
-        user_id = CustomUser.objects.filter(email='ghanteyyy@gmail.com')[0]
-
         allSubjects = self.GetSubjects()
 
-        results = Results(UserID=user_id, ProgrammeName=self.Programme)
-        results.save()
+        for _ in range(self.NumberOfResultsToGenerate):
+            correct_counter = 0
 
-        for subject in allSubjects:
-            total_questions_per_subject = subject.TotalQuestionsToSelect
-            number_of_correct_answers_to_select = random.randint(1, total_questions_per_subject)
-
-            correct_counter += number_of_correct_answers_to_select
-
-            questions = list(Questions.objects.filter(SubjectID=subject.ID))
-            random.shuffle(questions)
-
-            for num in range(total_questions_per_subject):
-                question = questions[num]
-                wrong_or_right = random.SystemRandom().choice([0, 1])
-
-                if number_of_correct_answers_to_select > 0 and wrong_or_right:
-                    user_answer = question.Answer
-                    number_of_correct_answers_to_select -= 1
-
-                else:
-                    choices = [question.OptionOne, question.OptionTwo, question.OptionThree, question.OptionFour]
-                    choices = [choice for choice in choices if choice != question.Answer]
-                    user_answer = random.choice(choices)
-
-                result_details = ResultDetails(
-                                    ResultID = results,
-                                    QuestionID = question,
-                                    UserAnswer = user_answer
-                                )
-
-                result_details.save()
-
-            results.CorrectCounter = correct_counter
+            results = Results(UserID=self.UsersObj, ProgrammeName=self.Programme)
             results.save()
+
+            for subject in allSubjects:
+                total_questions_per_subject = subject.TotalQuestionsToSelect
+                number_of_correct_answers_to_select = random.randint(1, total_questions_per_subject)
+
+                correct_counter += number_of_correct_answers_to_select
+
+                questions = list(Questions.objects.filter(SubjectID=subject.ID))
+                random.shuffle(questions)
+
+                for num in range(total_questions_per_subject):
+                    question = questions[num]
+                    wrong_or_right = random.SystemRandom().choice([0, 1])
+
+                    if number_of_correct_answers_to_select > 0 and wrong_or_right:
+                        user_answer = question.Answer
+                        number_of_correct_answers_to_select -= 1
+
+                    else:
+                        choices = [question.OptionOne, question.OptionTwo, question.OptionThree, question.OptionFour]
+                        choices = [choice for choice in choices if choice != question.Answer]
+                        user_answer = random.choice(choices)
+
+                    result_details = ResultDetails(
+                                        ResultID = results,
+                                        QuestionID = question,
+                                        UserAnswer = user_answer
+                                    )
+
+                    result_details.save()
+
+                results.CorrectCounter = correct_counter
+                results.save()
 
 
 class Command(BaseCommand):
@@ -65,7 +68,11 @@ class Command(BaseCommand):
     help = 'our help string comes here'
 
     def _create_tags(self):
-        PopulateResults('BCA').Action()
+        email = input("Enter user's email: ")
+        programme = input("Enter programme: ")
+        numberOfResults = int(input("Enter the number of results to generate: "))  # 40
+
+        PopulateResults(programme, email, numberOfResults).Action()
 
     def handle(self, *args, **options):
         self._create_tags()

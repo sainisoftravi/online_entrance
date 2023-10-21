eab558b05b8530a561e90daf3b6b47134ce5a844
ghanteyyy <ghanteyyy@gmail.com>
2023-10-21 16:41:10 +0545

    Added automatic resizing of the inner-wrapper div

---
+++ static/js/admin/resize-div-height.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    var divs = document.getElementsByClassName('inner-wrapper');
+    var maxHeight = 0;
+
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].clientHeight > maxHeight) {
+            maxHeight = divs[i].clientHeight;
+        }
+    }
+
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].style.height = maxHeight + "px";
+    }
+};

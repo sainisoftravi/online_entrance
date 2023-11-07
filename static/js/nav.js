a02fbaeb3aa43d4c7483dc30d40868ddf5b284b8
ghanteyyy <ghanteyyy@gmail.com>
2023-11-07 22:52:10 +0545

    Changed side-bar-menu to floating-menu

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,18 +1,23 @@
-logout_div = document.querySelector('.logout-link');
-logout_link = document.querySelector('.logout-link');
 profile_image = document.querySelector('.profile-img');
+detached_pop_up_menu = document.querySelector('.detached-pop-up');
 
 if(profile_image){
-    profile_image.addEventListener('mouseover', () => {
-        logout_div.classList.add('show-logout-link');
-        logout_div.classList.remove('hide-logout-link');
+    profile_image.addEventListener('click', () => {
+        if(detached_pop_up_menu.classList.contains('hide')){
+            detached_pop_up_menu.classList.remove('hide');
+        }
+
+        else{
+            detached_pop_up_menu.classList.add('hide');
+        }
     });
 
 
     document.addEventListener('click', (Event) => {
-        if(Event.target != logout_link){
-            logout_div.classList.remove('show-logout-link');
-            logout_div.classList.add('hide-logout-link');
+        element = Event.target;
+
+        if(element != detached_pop_up_menu && element != profile_image){
+            detached_pop_up_menu.classList.add('hide');
         }
     });
 }

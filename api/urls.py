d0985f4137346b7c9a75d0e40f8c9fdf74eb8596
ghanteyyy <ghanteyyy@gmail.com>
2023-12-08 10:18:06 +0545

    Added new endpoints

--- api/urls.py
+++ api/urls.py
@@ -3,13 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('api/exams', views.Exam.as_view()),
     path('api/users', views.Users.as_view()),
     path('api/reports', views.Reports.as_view()),
     path('api/subjects', views.Subjects.as_view()),
     path('api/questions', views.Question.as_view()),
     path('api/feedbacks', views.Feedbacks.as_view()),
     path('api/programmes', views.Programmes.as_view()),
+    path('api/users_exams', views.UsersExams.as_view()),
     path('api/exams/<str:get_by>', views.Exam.as_view()),
     path('api/users/<str:get_by>', views.Users.as_view()),
     path('api/reports/<str:get_by>', views.Reports.as_view()),
@@ -17,4 +17,6 @@ urlpatterns = [
     path('api/questions/<str:get_by>', views.Question.as_view()),
     path('api/histories/<str:get_by>', views.Histories.as_view()),
     path('api/programmes/<str:get_by>', views.Programmes.as_view()),
+    path('api/exams/<str:user_id>/<str:programme>', views.Exam.as_view()),
+    path('api/users_exams_each_programmes/<str:user_id>', views.UsersExamsInEachProgramme.as_view()),
 ]

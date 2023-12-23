19115dcf4c5336e182f143bcad93aa5372c5ca27
ghanteyyy <ghanteyyy@gmail.com>
2023-12-23 11:01:52 +0545

    Improved email validation

--- static/js/login.js
+++ static/js/login.js
@@ -4,15 +4,16 @@ password_box = document.querySelector('#password');
 email_error = document.querySelector('.email-error');
 submit_button = document.querySelector('.submit-btn');
 password_error = document.querySelector('.password-error');
+email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+
 
 function validateForm(){
     success = true;
-    email_regex = /[a-z0-9]+@[a-z]+[.][a-z]/i;
 
     password = password_box.value;
     email_value = email_box.value;
 
-    if(!email_value || email_value.indexOf(' ') != -1 || (email_value.indexOf('@') != -1 && email_regex.test(email_value) == false)){
+    if(!email_regex.test(email_value)){
         success = false;
         email_error.innerHTML = 'Invalid Email';
         email_error.classList.add('show-error');
